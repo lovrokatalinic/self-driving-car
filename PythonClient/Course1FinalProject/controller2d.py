f12f8871f92d8eea5de5ce42429be6f04b948805
lovrokatalinic <lovro.katalinic@gmail.com>
2021-11-01 19:56:55 +0100

    Last assignment of introduction to self-driving car'

---
+++ PythonClient/Course1FinalProject/controller2d.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+
+"""
+2D Controller Class to be used for the CARLA waypoint follower demo.
+"""
+
+import cutils
+import numpy as np
+
+class Controller2D(object):
+    def __init__(self, waypoints):
+        self.vars                = cutils.CUtils()
+        self._current_x          = 0
+        self._current_y          = 0
+        self._current_yaw        = 0
+        self._current_speed      = 0
+        self._desired_speed      = 0
+        self._current_frame      = 0
+        self._current_timestamp  = 0
+        self._start_control_loop = False
+        self._set_throttle       = 0
+        self._set_brake          = 0
+        self._set_steer          = 0
+        self._waypoints          = waypoints
+        self._conv_rad_to_steer  = 180.0 / 70.0 / np.pi
+        self._pi                 = np.pi
+        self._2pi                = 2.0 * np.pi
+
+    def update_values(self, x, y, yaw, speed, timestamp, frame):
+        self._current_x         = x
+        self._current_y         = y
+        self._current_yaw       = yaw
+        self._current_speed     = speed
+        self._current_timestamp = timestamp
+        self._current_frame     = frame
+        if self._current_frame:
+            self._start_control_loop = True
+            
+    def get_closest_waypoint_index(self):
+        min_idx       = 0
+        min_dist      = float("inf")
+        for i in range(len(self._waypoints)):
+            dist = np.linalg.norm(np.array([
+                    self._waypoints[i][0] - self._current_x,
+                    self._waypoints[i][1] - self._current_y]))
+            if dist < min_dist:
+                min_dist = dist
+                min_idx = i
+        return min_idx
+
+    def update_desired_speed(self):
+        min_idx       = 0
+        min_dist      = float("inf")
+        desired_speed = 0
+        for i in range(len(self._waypoints)):
+            dist = np.linalg.norm(np.array([
+                    self._waypoints[i][0] - self._current_x,
+                    self._waypoints[i][1] - self._current_y]))
+            if dist < min_dist:
+                min_dist = dist
+                min_idx = i
+        if min_idx < len(self._waypoints)-1:
+            desired_speed = self._waypoints[min_idx][2]
+        else:
+            desired_speed = self._waypoints[-1][2]
+        self._desired_speed = desired_speed
+
+    def update_waypoints(self, new_waypoints):
+        self._waypoints = new_waypoints
+
+    def get_commands(self):
+        return self._set_throttle, self._set_steer, self._set_brake
+
+    def set_throttle(self, input_throttle):
+        # Clamp the throttle command to valid bounds
+        throttle           = np.fmax(np.fmin(input_throttle, 1.0), 0.0)
+        self._set_throttle = throttle
+
+    def set_steer(self, input_steer_in_rad):
+        # Covnert radians to [-1, 1]
+        input_steer = self._conv_rad_to_steer * input_steer_in_rad
+
+        # Clamp the steering command to valid bounds
+        steer           = np.fmax(np.fmin(input_steer, 1.0), -1.0)
+        self._set_steer = steer
+
+    def set_brake(self, input_brake):
+        # Clamp the steering command to valid bounds
+        brake           = np.fmax(np.fmin(input_brake, 1.0), 0.0)
+        self._set_brake = brake
+
+    def update_controls(self):
+        ######################################################
+        # RETRIEVE SIMULATOR FEEDBACK
+        ######################################################
+        x               = self._current_x
+        y               = self._current_y
+        yaw             = self._current_yaw
+        v               = self._current_speed
+        self.update_desired_speed()
+        v_desired       = self._desired_speed
+        t               = self._current_timestamp
+        waypoints       = self._waypoints
+        throttle_output = 0
+        steer_output    = 0
+        brake_output    = 0
+
+        ######################################################
+        ######################################################
+        # MODULE 7: DECLARE USAGE VARIABLES HERE
+        ######################################################
+        ######################################################
+        """
+            Use 'self.vars.create_var(<variable name>, <default value>)'
+            to create a persistent variable (not destroyed at each iteration).
+            This means that the value can be stored for use in the next
+            iteration of the control loop.
+
+            Example: Creation of 'v_previous', default value to be 0
+            self.vars.create_var('v_previous', 0.0)
+
+            Example: Setting 'v_previous' to be 1.0
+            self.vars.v_previous = 1.0
+
+            Example: Accessing the value from 'v_previous' to be used
+            throttle_output = 0.5 * self.vars.v_previous
+        """
+        self.vars.create_var('v_previous', 0.0)
+        self.vars.create_var('v_error_previous', 0.0)
+        self.vars.create_var('integral_term_previous', 0.0)
+        self.vars.create_var('closest_waypoint_previous', waypoints[0])
+        self.vars.create_var('t_previous', 0.0)
+        self.vars.create_var('K_P', 1)
+        self.vars.create_var('K_I', 0.2)
+        self.vars.create_var('K_D', 0.08)
+        self.vars.create_var('k', 2.5)
+        self.vars.create_var('k_s', 0.01)
+
+        # Skip the first frame to store previous values properly
+        if self._start_control_loop:
+            """
+                Controller iteration code block.
+
+                Controller Feedback Variables:
+                    x               : Current X position (meters)
+                    y               : Current Y position (meters)
+                    yaw             : Current yaw pose (radians)
+                    v               : Current forward speed (meters per second)
+                    t               : Current time (seconds)
+                    v_desired       : Current desired speed (meters per second)
+                                      (Computed as the speed to track at the
+                                      closest waypoint to the vehicle.)
+                    waypoints       : Current waypoints to track
+                                      (Includes speed to track at each x,y
+                                      location.)
+                                      Format: [[x0, y0, v0],
+                                               [x1, y1, v1],
+                                               ...
+                                               [xn, yn, vn]]
+                                      Example:
+                                          waypoints[2][1]: 
+                                          Returns the 3rd waypoint's y position
+
+                                          waypoints[5]:
+                                          Returns [x5, y5, v5] (6th waypoint)
+                
+                Controller Output Variables:
+                    throttle_output : Throttle output (0 to 1)
+                    steer_output    : Steer output (-1.22 rad to 1.22 rad)
+                    brake_output    : Brake output (0 to 1)
+            """
+
+            ######################################################
+            ######################################################
+            # MODULE 7: IMPLEMENTATION OF LONGITUDINAL CONTROLLER HERE
+            ######################################################
+            ######################################################
+            """
+                Implement a longitudinal controller here. Remember that you can
+                access the persistent variables declared above here. For
+                example, can treat self.vars.v_previous like a "global variable".
+            """
+            v_error = v_desired - v
+            dt      = t - self.vars.t_previous
+            dverror = v_error - self.vars.v_error_previous
+            
+            proportional_term =  v_error
+            integral_term     =  self.vars.integral_term_previous + (self.vars.v_error_previous + dverror/2) * dt
+            derivative_term   =  dverror / dt
+            acc = (self.vars.K_P*proportional_term) + (self.vars.K_I*integral_term) + (self.vars.K_D*derivative_term)
+            
+            # Change these outputs with the longitudinal controller. Note that
+            # brake_output is optional and is not required to pass the
+            # assignment, as the car will naturally slow down over time.
+            throttle_output = np.tanh(acc)
+            brake_output    = 0
+
+            ######################################################
+            ######################################################
+            # MODULE 7: IMPLEMENTATION OF LATERAL CONTROLLER HERE
+            ######################################################
+            ######################################################
+            """
+                Implement a lateral controller here. Remember that you can
+                access the persistent variables declared above here. For
+                example, can treat self.vars.v_previous like a "global variable".
+            """
+            closest_waypoint_idx = self.get_closest_waypoint_index()
+            closest_waypoint = waypoints[closest_waypoint_idx]
+            closest_waypoint_previous = self.vars.closest_waypoint_previous
+            
+            # Calculate crosstrack error as minimal distance of current position (x, y) and line defined by current and previous closest waypoint
+            if closest_waypoint != closest_waypoint_previous:
+                closest_waypoints_dist = (closest_waypoint[0] - closest_waypoint_previous[0])**2 + (closest_waypoint[1] - closest_waypoint_previous[1])**2
+                crosstrack_error       = ((closest_waypoint[0] - closest_waypoint_previous[0])*(closest_waypoint_previous[1] - y) - \
+                                          (closest_waypoint_previous[0] - x)*(closest_waypoint[1] - closest_waypoint_previous[1])) / np.sqrt(closest_waypoints_dist)
+            else:
+                crosstrack_error = 0
+                             
+            # Calculate headline error as angle between yaw angle and angle of line defined by current and previous closest waypoint
+            ego_vector = (np.cos(yaw), np.sin(yaw))
+            ref_vector = (closest_waypoint[0] - closest_waypoint_previous[0], closest_waypoint[1] - closest_waypoint_previous[1])
+            headline_error = np.arctan2(ego_vector[0]*ref_vector[1] - ego_vector[1]*ref_vector[0], ego_vector[0]*ref_vector[0] + ego_vector[1]*ref_vector[1])
+
+            # Calculate steering angle with formula given in course
+            steering_angle = np.arctan(self.vars.k*crosstrack_error/(v + self.vars.k_s)) + headline_error
+            
+            # Change the steer output with the lateral controller. 
+            steer_output = steering_angle
+
+            ######################################################
+            # SET CONTROLS OUTPUT
+            ######################################################
+            self.set_throttle(throttle_output)  # in percent (0 to 1)
+            self.set_steer(steer_output)        # in rad (-1.22 to 1.22)
+            self.set_brake(brake_output)        # in percent (0 to 1)
+
+        ######################################################
+        ######################################################
+        # MODULE 7: STORE OLD VALUES HERE (ADD MORE IF NECESSARY)
+        ######################################################
+        ######################################################
+        """
+            Use this block to store old values (for example, we can store the
+            current x, y, and yaw values here using persistent variables for use
+            in the next iteration)
+        """
+        self.vars.v_previous = v  # Store forward speed to be used in next step
+        self.vars.v_error_previous = v_error
+        self.vars.integral_term_previous = integral_term
+        self.vars.closest_waypoint_previous = closest_waypoint
+        self.vars.t_previous = t
